a2ef78024ee20941e2404a5e31791ce75e9b18b2
Dmitri <dimae4552@gmail.com>
2026-05-14 12:15:39 +0500

    feat: implement application form flow

--- app/keyboards.py
+++ app/keyboards.py
@@ -5,3 +5,10 @@ register_keyboard = InlineKeyboardMarkup(
         [InlineKeyboardButton(text="📝 Подать заявку", callback_data="submit_application")],
     ]
 )
+
+
+rules_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="✅ Я прочитал правила", callback_data="rules_read")]
+    ]
+)
--- app/register_handlers.py
+++ app/register_handlers.py
@@ -35,3 +35,54 @@ async def cmd_submit(callback: CallbackQuery, state: FSMContext):
 async def process_nickname(message: Message, state: FSMContext):
     nickname = message.text
     await state.update_data(nickname=nickname)
+
+    await state.set_state(ApplicationForm.age)
+    await message.answer("🎂 Сколько тебе лет?")
+
+
+@register_router.message(ApplicationForm.age)
+async def process_age(message: Message, state: FSMContext):
+    age = message.text
+    await state.update_data(age=age)
+
+    await state.set_state(ApplicationForm.experience)
+    await message.answer("⛏ Сколько лет играешь в Minecraft?")
+
+
+@register_router.message(ApplicationForm.experience)
+async def process_experience(message: Message, state: FSMContext):
+    experience = message.text
+    await state.update_data(experience=experience)
+
+    await state.set_state(ApplicationForm.plans)
+    await message.answer("🗺 Какие у тебя планы на сервере? Расскажи пару слов")
+
+
+@register_router.message(ApplicationForm.plans)
+async def process_plans(message: Message, state: FSMContext):
+    plans = message.text
+    await state.update_data(plans=plans)
+
+    await state.set_state(ApplicationForm.rp_situation)
+    await message.answer(
+        "💎 Игрок оставил незапертый сундук с алмазами в общей зоне. Твои действия?"
+    )
+
+
+@register_router.message(ApplicationForm.rp_situation)
+async def process_situation(message: Message, state: FSMContext):
+    rp_situation = message.text
+    await state.update_data(rp_situation=rp_situation)
+
+    await state.set_state(ApplicationForm.rules)
+    await message.answer("Правила сервера\nЗаглушка", reply_markup=kb.rules_keyboard)
+
+
+@register_router.callback_query(ApplicationForm.rules, F.data == "rules_read")
+async def rules_confirmed(callback: CallbackQuery, state: FSMContext):
+    await state.update_data(rules=True)
+    data = await state.get_data()
+    await state.clear()
+
+    await callback.message.edit_text("✅ Заявка отправлена! Ждём решения админов 🎉")
+    await callback.answer()
